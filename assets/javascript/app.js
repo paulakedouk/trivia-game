af45ade2fe4d9731e35c61c310795edb472c01e9
paulakedouk <paulakedouk@gmail.com>
2018-01-29 23:48:56 -0800

    Finishing the game screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -69,7 +69,7 @@ var questions = [
 var game;
 var counter = 0;
 var clock;
-var timer = 10;
+var timer = 30;
 var correctCounter = 0;
 var incorrectCounter = 0;
 var unansweredCounter = 0;
@@ -77,13 +77,13 @@ var unansweredCounter = 0;
 $(document).ready(function() {
   // Start the game when that start button is clicked
   $('.answers').css('visibility', 'hidden');
-  $('body').on('click', '.start-btn', function(e) {
+  $('body').on('click', '.start-btn', function(event) {
     event.preventDefault();
     startGame();
     $('.answers').css('visibility', 'visible');
   });
 
-  $('body').on('click', '.answer', function() {
+  $('body').on('click', '.answer', function(event) {
     // console.log($(this));
     chosenAnswer = $(this).text();
     var answerCounter = questions[counter].answers;
@@ -93,80 +93,109 @@ $(document).ready(function() {
       if (chosenAnswer === answerCounter[i].answer && answerCounter[i].value === true) {
         clearInterval(clock);
         $(this).attr('class', 'right-answer answer');
-        setTimeout(questionCounter, 3000);
+        rightAnswer();
       } else if (chosenAnswer === answerCounter[i].answer && answerCounter[i].value === false) {
         clearInterval(clock);
         $(this).attr('class', 'wrong-answer answer');
-        setTimeout(questionCounter, 3000);
+        wrongAnswer();
       }
     }
   });
 
-  function rightAnswer() {
-    correctCounter++;
-    $('.time').html(timer);
-    $('.score').text('Right answers: ' + correctCounter);
-    setTimeout(questionCounter, 4000);
-  }
-
-  function wrongAnswer() {
-    incorrectCounter++;
-    $('.time').html(timer);
-    $('.score').text('Wrong answers: ' + incorrectCounter);
-    setTimeout(questionCounter, 4000);
-  }
-
-  function unanswered() {
-    unanswered++;
-    $('.main').append("<p class='times-up'>Time's up!</p>");
-    $('.right-answer').css('background-color', 'green');
-    setTimeout(questionCounter, 4000);
-  }
-
-  // Start the game
-  function startGame() {
-    $('.start-page').css('display', 'none');
-    $('.questions-page').css('visibility', 'visible');
-    $('.timer').html('<p>Time remaining: <span class="time">30</span></p>');
-
-    $('.question').html(questions[counter].question);
-    var showingAnswers =
-      '<p class="answer first-answer">' +
-      questions[counter].answers[0].answer +
-      '</p><p class="answer">' +
-      questions[counter].answers[1].answer +
-      '</p><p class="answer">' +
-      questions[counter].answers[2].answer +
-      '</p><p class="answer">' +
-      questions[counter].answers[3].answer +
-      '</p>';
-
-    $('.answers').html(showingAnswers);
-
-    timerHolder();
-  }
-
-  function questionCounter() {
-    if (counter < 7) {
-      counter++;
-      startGame();
-      timer = 10;
-      timerHolder();
-    }
-  }
-
-  // Timer function
-  function timerHolder() {
-    clearInterval(clock);
-    clock = setInterval(seconds, 1000);
-    function seconds() {
-      if (timer > 0) {
-        timer--;
-      } else if (timer === 0) {
-        clearInterval(clock);
-        unanswered();
-      }
-      $('.time').html(timer);
-    }
-  }
+  $('body').on('click', '.reset-button', function(event) {
+    event.preventDefault();
+    resetGame();
+  });
 });
+
+function rightAnswer() {
+  correctCounter++;
+  $('.time').html(timer);
+  $('.right').html('<p>Right answers: ' + correctCounter + '</p><br>');
+  setTimeout(questionCounter, 2000);
+}
+
+function wrongAnswer() {
+  incorrectCounter++;
+  $('.time').html(timer);
+  $('.wrong').html('<p>Wrong answers: ' + incorrectCounter + '</p>');
+  setTimeout(questionCounter, 2000);
+}
+
+function unanswered() {
+  unanswered++;
+  $('.main').append("<p class='times-up'>Time's up!</p>");
+  $('.right-answer').css('background-color', 'green');
+  setTimeout(questionCounter, 2000);
+}
+
+// Start the game
+function startGame() {
+  $('.start-page').css('display', 'none');
+  $('.questions-page').css('visibility', 'visible');
+  $('.timer').html('<p>Time remaining: <span class="time">30</span></p>');
+
+  $('.question').html(questions[counter].question);
+  var showingAnswers =
+    '<p class="answer first-answer">' +
+    questions[counter].answers[0].answer +
+    '</p><p class="answer">' +
+    questions[counter].answers[1].answer +
+    '</p><p class="answer">' +
+    questions[counter].answers[2].answer +
+    '</p><p class="answer">' +
+    questions[counter].answers[3].answer +
+    '</p>';
+
+  $('.answers').html(showingAnswers);
+
+  timerHolder();
+}
+
+function questionCounter() {
+  if (counter < 6) {
+    counter++;
+    startGame();
+    timer = 30;
+    timerHolder();
+  } else {
+    finishGame();
+  }
+}
+
+// Timer function
+function timerHolder() {
+  clearInterval(clock);
+  clock = setInterval(seconds, 1000);
+  function seconds() {
+    if (timer === 0) {
+      clearInterval(clock);
+      unanswered();
+    } else if (timer > 0) {
+      timer--;
+    }
+    $('.time').html(timer);
+  }
+}
+
+// Finishing the game
+function finishGame() {
+  var final = $('.main')
+    .html("<p>All done, here's how you did!<p><br><br>")
+    .append('<p>Correct Answers: ' + correctCounter + '</p><br>')
+    .append('<p>Wrong Answers: ' + incorrectCounter + '</p>');
+  $(final).attr('<div>');
+  $(final).attr('class', 'final');
+  $('.final').append('<p><a class="btn btn-primary btn-lg reset-button" href="#">Restart the game!</a></p>');
+}
+
+// Reset the game
+function resetGame() {
+  counter = 0;
+  correctCounter = 0;
+  incorrectCounter = 0;
+  unansweredCounter = 0;
+  timer = 30;
+  startGame();
+  timerHolder();
+}
